65b3e75983e8ff55a152553e8ec99d4014ef4a04
noopy <young_ahn@yonsei.ac.kr>
2020-02-24 02:30:52 +0900

    babel, nodemon config fin

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+var app = express();
+
+const PORT = 8000;
+
+const handleListening = () =>
+  console.log(`Listening in port http://localhost:${PORT}`);
+
+// request is req, and respond is res
+const handleHome = (req, res) => res.send("Hello from Django");
+
+// arrow function on javascript
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+// routing with "/"
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+// establishing server at port number 4000
+app.listen(8000, handleListening);
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/", function(req, res) {
+  res.send("hello world");
+});
